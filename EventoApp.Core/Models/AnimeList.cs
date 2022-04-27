279ca6306757bb2f28353d4bdf24eaebb0b1f3e8
jackvadderr <63818417+jackvadderr@users.noreply.github.com>
2022-04-27 06:39:44 -0700

    Arquivo C# externos

---
+++ EventoApp.Core/Models/AnimeList.cs
@@ -0,0 +1,11 @@
+﻿namespace EventoApp.Core.Models;
+
+public class MyUltimateAnimeList
+{
+    public int Rank { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Score { get; set; }
+
+}
---
+++ EventoApp.Core/Models/FilmeList.cs
@@ -0,0 +1,11 @@
+﻿namespace EventoApp.Core.Models;
+
+internal class MyUltimateFilmList
+{
+    public int Rank { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Score { get; set; }
+
+}
